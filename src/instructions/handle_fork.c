74607b964a675b655d3c44153db8841c3d5bd821
Esteban <esteban.peroz@epitech.eu>
2025-05-13 13:16:52 +0200

    Feat!: add lfork

--- src/instructions/handle_fork.c
+++ src/instructions/handle_fork.c
@@ -16,7 +16,7 @@ int handle_fork(instructions_params_t *params)
     new_adress = params->champ->prog_counter + new_adress % IDX_MOD;
     fill_struct_champions(params->champ->file,
         &params->vm->champion, params->champ->prog_id, new_adress);
-    params->champ->cylces_to_wait = op_tab[FORK_ID].nbr_cycles;
+    params->champ->cylces_to_wait = op_tab[LFORK_ID].nbr_cycles;
     params->champ->prog_counter += IND_SIZE;
     return 1;
 }
