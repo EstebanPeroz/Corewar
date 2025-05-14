2b2a505b0de6795ec0e8a3dcc26002d035b56434
Esteban <esteban.peroz@epitech.eu>
2025-05-14 15:42:05 +0200

    Feat: add tests for lfork

--- tests/test_fork.c
+++ tests/test_fork.c
@@ -40,3 +40,21 @@ Test(corewar, test_handle_fork, .init = redirect_all_std)
     cr_assert_eq(vm->champion->next->next->carry, 0);
 }
 
+Test(corewar, test_handle_lfork, .init = redirect_all_std)
+{
+    char *av[] = {"./corewar", "tests/pdd.cor", "-n", "3", "tests/zjmper.cor", NULL};
+    virtual_machine_t *vm = init_virtual_machine(0, 0);
+    
+    fill_vm(5, av, vm);
+    vm->champion->prog_counter += fork_call;
+    instructions_params_t params = { vm, 1, vm->champion, 0x0c, 1 , {0}, {0} };
+    if (vm->arena[vm->champion->prog_counter] != 0x0c) {
+        printf("Current instruction: %i\n", vm->arena[vm->champion->prog_counter]);
+        fflush(NULL);
+        cr_assert_fail("Wrong instruction\n");
+    }
+    handle_lfork(&params);
+    cr_assert_eq(vm->champion->carry, 0);
+    // Checking the forked champ
+    cr_assert_eq(vm->champion->next->next->carry, 0);
+}
