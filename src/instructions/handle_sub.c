1f5b6c74cbfbdca3a8570768d2373082b7189456
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2025-05-14 19:28:53 +0200

    feat(sub): add the sub instruction, use define in add and sub instr functions for more clarity

--- src/instructions/handle_sub.c
+++ src/instructions/handle_sub.c
@@ -2,12 +2,12 @@
 ** EPITECH PROJECT, 2025
 ** Corewar
 ** File description:
-** handle_add
+** handle_sub
 */
 
 #include "corewar.h"
 
-int handle_add(instructions_params_t *params)
+int handle_sub(instructions_params_t *params)
 {
     int reg1_content = 0;
     int reg2_content = 0;
@@ -22,7 +22,7 @@ int handle_add(instructions_params_t *params)
         return EXIT_FAILURE;
     reg1_content = params->champ->registers[params->values[0] - 1];
     reg2_content = params->champ->registers[params->values[1] - 1];
-    result = reg1_content ADDITION_MARK reg2_content;
+    result = reg1_content SUBSTRACTION_MARK reg2_content;
     params->champ->carry = (result == 0) ? 1 : 0;
     params->champ->registers[params->values[2] - 1] = result;
     return 0;
