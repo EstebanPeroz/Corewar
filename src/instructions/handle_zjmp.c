332952daebab06302036dd25db321d62a093276d
Esteban <esteban.peroz@epitech.eu>
2025-05-16 18:38:28 +0200

    Fix: zjmp nows move the PC on error

--- src/instructions/handle_zjmp.c
+++ src/instructions/handle_zjmp.c
@@ -6,14 +6,16 @@
 */
 #include "corewar.h"
 #include "op.h"
-#include <stdio.h>
 
 int handle_zjmp(instructions_params_t *params)
 {
     short index;
 
-    if (params->champ->carry != 1)
+    if (params->champ->carry != 1) {
+        params->champ->prog_counter += (1 + IND_SIZE) % IDX_MOD;
+        params->champ->prog_counter %= MEM_SIZE;
         return 0;
+    }
     index = read_bytes(params->vm->arena,
         params->champ->prog_counter + 1, IND_SIZE);
     params->champ->prog_counter += index % IDX_MOD;
